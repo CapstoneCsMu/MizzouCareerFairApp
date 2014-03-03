6549d68e61ceae7b3f8484b029fb5cc1c798fe22
CapstoneMU <mcwrmd@mail.missouri.edu>
2014-03-03 15:42:04 -0600

    Added Mobile Front end
    Added jquery mobile and created mobile.php
    Changed some CSS

---
+++ mizzouCareerFair/mobile.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html> 
+<html> 
+  <head> 
+  <!--
+       <meta charset="utf-8"> 
+       <title>Mizzou Career Fairs</title>
+       <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css">
+       <script src="http://code.jquery.com/jquery-1.8.2.min.js"></script> 
+       <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js"></script>
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+ -->
+ 
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/themes/MizzouCareerFair.css" />
+  <link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
+  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.1/jquery.mobile.structure-1.4.1.min.css" /> 
+  <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script> 
+  <script src="http://code.jquery.com/mobile/1.4.1/jquery.mobile-1.4.1.min.js"></script> 
+ 
+ 
+ </head> 
+ <body> 
+    <div data-role="page" data-theme="a"> 
+         <div data-role="header"> 
+               <h1>jQuery Mobile</h1> 
+         </div> 
+         <div data-role="content">      
+               <ul data-role="listview" data-inset="true" data-dividertheme="b"> 
+                     <li data-role="list-divider">Options</li> 
+                     <li><a href="option1.html">Companies</a></li> 
+                     <li><a href="option2.html">Fair Map</a></li> 
+                     <li><a href="option3.html">Events</a></li> 
+                     <li><a href="option4.html">Announcements</a></li> 
+					 <li><a href="option5.html">Career Fair 101</a></li>
+					 <li><a href="option6.html">Support</a></li>
+					 <li><a href="option7.html">Fairs</a></li>
+					 <li><a href="option8.html">Updates</a></li>
+               </ul>           
+         </div>   
+        <div data-role="footer">
+         <h4>&copy; 2014 Team X Mizzou Career Fair Web App</h4>
+        </div> 
+    </div> 
+    </body> 
+    </html>
