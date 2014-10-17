24c5a7c5f099a982e99d0adf76935d94aa5c4f30
Cecilia Martinez <dcm53f@mail.missouri.edu>
2014-10-17 18:18:18 -0500

    Upload Resume

--- mizzouCareerFair/updateProfile.php
+++ mizzouCareerFair/updateProfile.php
@@ -1,151 +1,95 @@
 <?php
-	include('check_https.php');
-	session_start();
-	$_POST['student_loggedin'] = $_SESSION['student_loggedin'];
-		//Include Database information
-		if($_SERVER['HTTP_HOST'] == 'localhost')
-			include('data_ryanslocal.php');
-		else
-			include ("data.php");
-		$conn = pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD) or die('Could not connect:'. pg_last_error());
-		if (!$conn) 
-		{
-			echo "<br/>An error occurred with connecting to the server.<br/>";
-			die();
-		}
-
-		//Run variables against dB
-		$query = array( 0 =>"SELECT * FROM careerschema.students WHERE email=$1");
-
-		//Search the three tables for authentication success
-		$userWasFound = FALSE;
-			
-		for ($p=0; $p<count($query);$p++)
-		{
-			$stmt = pg_prepare($conn, "check_".$p, $query[$p])  or die( "ERROR:". pg_last_error() );
-			$result = pg_execute($conn, "check_".$p, array(htmlspecialchars($_POST['email'])))  or die( "ERROR:". pg_last_error() );
-			if(pg_num_rows($result) > 0)
-			{
-				$userWasFound = TRUE;
-				break;
-			}
-		}
-		if (!$userWasFound)
-		{
-			//code here 
-		}
-		else
-		{	
-			$row = pg_fetch_assoc($result);
-			
-			$firstname = $row['firstame'];
-			$lastname = $row['lastname'];
-			$email = $row['email'];
-			$gradDate = $row['graddate'];
-			$major = $row['major'];
-			if (($row['resume']) != ""){
-				$resume = "Resume on file";
-			}
-			$phone = $row['phone'];
-			$goals = $row['lifeplan'];
-			$linkedin = $row['linkedin_id'];
-			//Cecilia: Left here 10/7/14 will finish the db insertion and update
-		}
-		
- ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Mizzou Career Fairs
-    </title> 
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1" name="viewport">
-    
-    <!-- Include CSS and JQM CSS -->
-    <link href="css/themes/MizzouCareerFair.css" rel="stylesheet">
-    <link href="css/themes/jquery.mobile.icons.min.css" rel="stylesheet">
-
-	<link href="jquery.mobile-1.4.4/jquery.mobile.structure-1.4.4.min.css" rel="stylesheet">
-    
-	<link rel="stylesheet" media="screen and (min-device-width: 800px)" href="css/themes/screensize.css"/>
+include('check_https.php');
+	//session_start();
+	//$_POST['student_loggedin'] = $_SESSION['student_loggedin'];
 	
-    <!-- Include jQuery and jQuery Mobile CDN, add actual files -->
-	<script src="js/jquery-1.11.1.min.js"></script>
-    <script src="jquery.mobile-1.4.4/jquery.mobile-1.4.4.min.js"></script>
-    <!-- Include JS file for our JS -->
-    <script src="js/index.js"></script>
-    
-    <!-- Include LinkedIn Framework, API Key Unique to Us -->
-	<?php if($_SERVER['HTTP_HOST'] == 'localhost'): ?>
-		<script type="text/javascript" src="https://platform.linkedin.com/in.js">
-  		api_key: 750nr1ytn6d9bz
-  		onLoad: onLinkedInLoad
-  		authorize: true
-	</script>
-	<?php else: ?>
-	<script type="text/javascript" src="https://platform.linkedin.com/in.js">
-  		api_key: 75a6k7ahbjlrny
-  		onLoad: onLinkedInLoad
-  		authorize: true
-	</script>
-	<?php endif; ?>
-	<!-- Include Google Maps API -->
-	<script type="text/javascript" src="https://maps.google.com/maps/api/js?v=3&sensor=false&language=en"></script>
-
-	<script type="text/javascript" src="index.js"></script>
-
-</head>
-
-<body>
-	<div data-role="page" data-dialog="true">
-			<div data-role="header">
-				<a data-icon="delete" data-transition="slideup" data-iconpos="notext" href="index.php">Back</a> 
-				<h1>Update Profile</h1>
-			</div>
+if (isset($_POST['Upload'])){
+	//print_r($_POST);
+	//print_r($_FILES['resume']['tmp_name']);
+	//exit();
+	$email = $_POST['email'];
+	if (is_uploaded_file($_FILES['resume']['tmp_name']) )
+	{
+		if ($_FILES['resume']['error'] !== UPLOAD_ERR_OK) 
+		{
+			die("Upload failed with error " . $_FILES['resume']['error']);
+		}
+		else{
+		
+			$finfo = finfo_open(FILEINFO_MIME_TYPE);
+			//grabing the mime type (server side)
+			$mime = finfo_file($finfo, $_FILES['resume']['tmp_name']);
 			
-			<div data-role="main" class="ui-content">
-				<form id="updateInfoForm" method="post" action="updateProfile.php" data-ajax="false">
-					<div class="ui-field-contain">
-						<div data-role="fieldcontain">
-							<label for="firstname">Firstname:</label>
-							<input type="text" name="firstname" id="firstname" value="<?php echo $firstname; ?>"data-mini="true">
-						</div>
-						<div data-role="fieldcontain">
-							<label for="lastname">Lastname:</label>
-							<input type="text" name="lastname" id="lastname" value="<?php echo $lastname; ?>" data-mini="true">
-						</div>
-						<div data-role="fieldcontain">
-							<label for="email">E-mail:</label>
-							<input type="email" name="email" id="email" value="<?php echo $email; ?>"data-mini="true">
-						</div>
-						<div data-role="fieldcontain">
-							<label for="gradDate">Grad. Date:</label>
-							<input type="text" name="gradDate" id="gradDate" value="<?php echo $gradDate; ?>"placeholder="Dec-2015" data-mini="true">
-						</div>
-						<div data-role="fieldcontain">
-							<label for="major">Major:</label>
-							<input type="text" name="major" id="major" value="<?php echo $major; ?>"data-mini="true">
-						</div>
-						<div data-role="fieldcontain">	
-							<label for="resume">Resume:</label>
-							<input type="file" name="resume" id="resume" value="<?php echo $resume; ?>"data-mini="true">
-						</div>
-						<div data-role="fieldcontain">	
-							<label for="phone">Phone:</label>
-							<input type="text" name="phone" id="phone" value="<?php echo $phone; ?>"placeholder="(xxx)xxx-xxxx" data-mini="true">
-						</div>
-						<div data-role="fieldcontain">	
-							<label for="lifePlan">Career Goals:</label>
-							<textarea rows="5" name="lifePlan" wrap="physical" maxlength="200" data-mini="true"id ="textarea"value="<?php echo $goals; ?>"></textarea>
-						</div>
-						<div data-role="fieldcontain">
-							<label for="linkedIn">LinkedIn Email:</label>
-							<input type="text" name="linkedIn" id="linkedIn" value="<?php echo $linkedin; ?>" data-mini="true">
-						</div>	
-					</div>
-					<center><input type="submit" data-inline="true" value="Submit"></center>
-				</form>
-			</div>
-	</div>
-</body>
-</html>
+			//exit();
+			$ok = false;
+			$max_size = 300000;
+			$destination = "uploads/";
+			//allowed mime types
+			$valid_mime_types = array('text/plain', 'application/msword', 'application/zip', 'application/doc', 'application/pdf', 'application/x-pdf',
+			'application/acrobat', 'applications/vnd.pdf', 'text/pdf', 'text/x-pdf', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+			'application/vnd.openxmlformats-officedocument.wordprocessingml.template', 'application/vnd.ms-word.document.macroenabled.12', '.doc', '.pdf');
+			
+			//checking file's mime type is inside allowed mime types
+			if(in_array($mime, $valid_mime_types)){
+				//checking size less than 292KB
+				if($_FILES['resume']['size'] < $max_size)
+				{
+					echo "I like your file\n";
+					//UPLOADING RESUME
+					$d=date ("d");
+					$m=date ("m");
+					$y=date ("Y");
+					$t=time();
+					$dmt=$d+$m+$y+$t;    
+					$ran= rand(0,10000000);
+					$dmtran= $dmt+$ran;
+					$un=  uniqid();
+					$dmtun = $dmt.$un;
+					$newPath = md5($dmtran.$un);
+					$destination = $destination . $newPath;
+					
+					move_uploaded_file($_FILES['resume']['tmp_name'], $destination);
+					//might want to add the "http://babbage.cs.missouri.edu/~cs4970grp..." 
+					//to insert the entire path to the db but for now the path is uploads/[filename]
+					echo $destination;
+					
+									
+					header("Location: updateProfileForm.php");
+					exit();
+				}
+				else
+				{
+					echo "Your file is too large.  Please try again!";
+					header("Location: addResume.php");
+					exit();				
+				}		
+			}
+			else
+			{
+				echo "Please submit a .pdf or .doc";	
+				header("Location: updateForm.php");
+				exit();					
+			}	
+		}
+	}
+	//do this after the file is saved in the server
+	header("Location: updateProfileForm.php");
+}
+
+if(isset($_POST['Update'])){
+	print_r($_POST);
+	exit();
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$email = $_POST['email'];
+	$gradDate = $_POST['gradDate'];
+	$major = $_POST['major'];
+	$resume =  $_POST['resume'];
+	$phone = $_POST['phone'];
+	$lifePlan = $_POST['lifePlan'];
+	$linkedIn = $_POST['linkedIn'];
+	$student_loggedin = $_POST['student_loggedin'];
+	
+}
+
+?>
---
+++ mizzouCareerFair/updateProfileForm.php
@@ -0,0 +1,153 @@
+<?php
+	include('check_https.php');
+	session_start();
+	$_POST['student_loggedin'] = $_SESSION['student_loggedin'];
+	$email = $_POST['student_loggedin'];
+	//print_r($_POST);
+	//exit();
+		//Include Database information
+		if($_SERVER['HTTP_HOST'] == 'localhost')
+			include('data_ryanslocal.php');
+		else
+			include ("data.php");
+		$conn = pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD) or die('Could not emaiconnect:'. pg_last_error());
+		if (!$conn) 
+		{
+			echo "<br/>An error occurred with connecting to the server.<br/>";
+			die();
+		}
+
+		//Run variables against dB
+		$query = array( 0 =>"SELECT * FROM careerschema.students WHERE email=$1");
+
+		//Search the three tables for authentication success
+		$userWasFound = FALSE;
+			
+		for ($p=0; $p<count($query);$p++)
+		{
+			$stmt = pg_prepare($conn, "check_".$p, $query[$p])  or die( "ERROR:". pg_last_error() );
+			$result = pg_execute($conn, "check_".$p, array($email))  or die( "ERROR:". pg_last_error() );
+			if(pg_num_rows($result) > 0)
+			{
+				$userWasFound = TRUE;
+				break;
+			}
+		}
+		if (!$userWasFound)
+		{
+			//code here 
+			//header("Location: index.php");
+		}
+		else
+		{	
+			$row = pg_fetch_assoc($result);
+			
+			$firstname = $row['firstame'];
+			$lastname = $row['lastname'];
+			$email = $row['email'];
+			$gradDate = $row['graddate'];
+			$major = $row['major'];
+			if (($row['resume']) != ""){
+				$resume = "Resume on file";
+			}
+			$phone = $row['phone'];
+			$goals = $row['lifeplan'];
+			$linkedin = $row['linkedin_id'];
+		}
+		
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mizzou Career Fairs
+    </title> 
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    
+    <!-- Include CSS and JQM CSS -->
+    <link href="css/themes/MizzouCareerFair.css" rel="stylesheet">
+    <link href="css/themes/jquery.mobile.icons.min.css" rel="stylesheet">
+
+	<link href="jquery.mobile-1.4.4/jquery.mobile.structure-1.4.4.min.css" rel="stylesheet">
+    
+	<link rel="stylesheet" media="screen and (min-device-width: 800px)" href="css/themes/screensize.css"/>
+	
+    <!-- Include jQuery and jQuery Mobile CDN, add actual files -->
+	<script src="js/jquery-1.11.1.min.js"></script>
+    <script src="jquery.mobile-1.4.4/jquery.mobile-1.4.4.min.js"></script>
+    <!-- Include JS file for our JS -->
+    <script src="js/index.js"></script>
+    
+    <!-- Include LinkedIn Framework, API Key Unique to Us -->
+	<?php if($_SERVER['HTTP_HOST'] == 'localhost'): ?>
+		<script type="text/javascript" src="https://platform.linkedin.com/in.js">
+  		api_key: 750nr1ytn6d9bz
+  		onLoad: onLinkedInLoad
+  		authorize: true
+	</script>
+	<?php else: ?>
+	<script type="text/javascript" src="https://platform.linkedin.com/in.js">
+  		api_key: 75a6k7ahbjlrny
+  		onLoad: onLinkedInLoad
+  		authorize: true
+	</script>
+	<?php endif; ?>
+	<!-- Include Google Maps API -->
+	<script type="text/javascript" src="https://maps.google.com/maps/api/js?v=3&sensor=false&language=en"></script>
+
+	<script type="text/javascript" src="index.js"></script>
+
+</head>
+
+<body>
+	<div data-role="page" data-dialog="true">
+			<div data-role="header">
+				<a data-icon="delete" data-transition="slideup" data-iconpos="notext" href="index.php">Back</a> 
+				<h1>Update Profile</h1>
+			</div>
+			
+			<div data-role="main" class="ui-content">
+					<div class="ui-field-contain">
+				<form id="updateInfoForm" method="post" enctype ="multipart/form-data" action="updateProfile.php" data-ajax="false">
+						
+						<div data-role="fieldcontain">
+							<label for="firstname">Firstname:</label>
+							<input type="text" name="firstname" id="firstname" value="<?php echo $firstname; ?>"data-mini="true">
+						</div>
+						<div data-role="fieldcontain">
+							<label for="lastname">Lastname:</label>
+							<input type="text" name="lastname" id="lastname" value="<?php echo $lastname; ?>" data-mini="true">
+						</div>
+						<div data-role="fieldcontain">
+							<label for="email">E-mail:</label>
+							<input type="email" name="email" id="email" value="<?php echo $email; ?>"data-mini="true">
+						</div>
+						<div data-role="fieldcontain">
+							<label for="gradDate">Grad. Date:</label>
+							<input type="text" name="gradDate" id="gradDate" value="<?php echo $gradDate; ?>"placeholder="Dec-2015" data-mini="true">
+						</div>
+						<div data-role="fieldcontain">
+							<label for="major">Major:</label>
+							<input type="text" name="major" id="major" value="<?php echo $major; ?>"data-mini="true">
+						</div>
+						<div data-role="fieldcontain">	
+							<label for="phone">Phone:</label>
+							<input type="text" name="phone" id="phone" value="<?php echo $phone; ?>"placeholder="(xxx)xxx-xxxx" data-mini="true">
+						</div>
+						<div data-role="fieldcontain">	
+							<label for="lifePlan">Career Goals:</label>
+							<textarea rows="5" name="lifePlan" wrap="physical" maxlength="200" data-mini="true"id ="textarea"value="<?php echo $goals; ?>"></textarea>
+						</div>
+						<div data-role="fieldcontain">
+							<label for="linkedIn">LinkedIn Email:</label>
+							<input type="text" name="linkedIn" id="linkedIn" value="<?php echo $linkedin; ?>" data-mini="true">
+						</div>	
+					</div>
+					<input type="hidden" name="student_loggedin" value="<? echo $email;?>">
+					<center><input type="submit" data-inline="true" value="Update" name="Update"></center>
+					
+				</form>
+			</div>
+	</div>
+</body>
+</html>
