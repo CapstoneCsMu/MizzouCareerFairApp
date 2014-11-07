1f53b44b0de627d2353486013637c3d8dac30640
Stephen Schroeder <sosb8d@mail.missouri.edu>
2014-11-06 18:04:30 -0600

    Revert "fixed minor add admin bug"
    This reverts commit 510f06391c228a99bde70d17347f4f16e3f97f7e.

--- mizzouCareerFair/adminUsers.php
+++ mizzouCareerFair/adminUsers.php
@@ -3,7 +3,6 @@
 Parent:  login.php (redirects here upon admin logging in)
 Function:  administrative dashboard to configure application*/
 require_once('phpmailer/class.phpmailer.php');
-include("rssFunctions.php");
 session_start();
 if(!$_SESSION['admin_loggedin']){
     $_SESSION['admin_attempt'] = "yes";
@@ -82,7 +81,7 @@ if(!$_SESSION['admin_loggedin']){
                 </br>
         </div>
 </div>
-
+<!--
 <div data-role="page" id="addAdmin" data-dialog="true">
         <div data-role="header">
         </br>
@@ -101,14 +100,60 @@ if(!$_SESSION['admin_loggedin']){
                                         <input type="text" name="lastName" id="lastName">
                                         <label for="password"><b>Choose a Password:</label>
                                         <input type="password" name="password" id="password" placeholder="At least 5 characters">
-                                        <div class="submitBtn">
-                                            <input type="submit" name="submit" value="Submit">
-                                        </div>
                 </form>
+                        <center>-->
 
-                                <?php if (isset($_POST['submit'])){ handleNewAdmin();} ?>
+                                <!--<a href="admin.php#addAdmin" data-inline="true" data-role="button" onclick="submitAdmin();" value="Submit" >Submit</a>-->
+                               <!-- <input type="submit" data-inline="true" value="Add Admin" onclick="submitAdmin();" name="Submit">-->
+                                <?php if (isset($_POST['Submit'])){ handleNewAdmin();} ?>
+                                <!--<input type="submit" data-inline="true" onclick="submitAdmin()" value="Add Admin" name="Add Admin">-->
 
-
+<?php
+function handleNewAdmin()
+{
+        //print_r($_POST);
+        //exit();
+                if($_SERVER['HTTP_HOST'] == 'localhost')
+                        include('data_ryanslocal.php');
+                else
+                        include ("data.php");
+                $conn = pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD) or die('Could not connect:'. pg_last_error());
+                if (!$conn)
+                {
+                        echo "\n<div class='container'>\n\t<div class ='alert alert-danger'>";
+                        echo "<center>An Error occurred during connection.</center>";
+                        echo "\n\t</div>\n</div>";
+                        exit();
+                }
+                mt_srand(); //Seed number generator
+                $salt = mt_rand(); //generate salt
+                $salt = sha1($salt);
+                $salt = trim($salt);
+                $pass = htmlspecialchars($_POST['password']);
+                $email = htmlspecialchars($_POST['email']);
+                $firstName = $_POST['firstName'];
+                $lastName = $_POST['lastName'];
+                $passHashed = sha1($salt.$pass);
+                for ($i=0; $i<10000; $i++) //Slow Hashing
+                {
+                  $passHashed = sha1($passHashed);
+                }
+                 $query = "INSERT INTO careerschema.authorizationtable (email, firstname, lastname, hashed_pass, salt, user_type) VALUES ($1,$2,$3,$4,$5,$6)";
+                 $state = pg_prepare($conn,"insert_1",$query) ;
+                 $queryInsert = pg_execute($conn,"insert_1",array($email, $_POST['firstName'], $_POST['lastName'], $passHashed, $salt,"admin") )  ;
+                if ($queryInsert)
+                {
+                        //Kristi:  change to the appropiate action if the admin is inserted successfully
+                        header("Location: admin.php");//change this line
+                        exit();
+                }
+                else{
+                        //Kristi:  change to the appropiate action if the admin is not inserted
+			header("Location: admin.php#failure");//change this line
+                        exit();
+		}
+}
+?>
 
 <div data-role="page" id="deleteAdmin" data-dialog="true">
         <div data-role="header">
--- mizzouCareerFair/rssFunctions.php
+++ mizzouCareerFair/rssFunctions.php
@@ -24,48 +24,4 @@ function authorization(){
     }
 }
 
-function handleNewAdmin()
-{
-    //print_r($_POST);
-    //exit();
-    if($_SERVER['HTTP_HOST'] == 'localhost')
-        include('data_ryanslocal.php');
-    else
-        include ("data.php");
-    $conn = pg_connect(HOST." ".DBNAME." ".USERNAME." ".PASSWORD) or die('Could not connect:'. pg_last_error());
-    if (!$conn)
-    {
-        echo "\n<div class='container'>\n\t<div class ='alert alert-danger'>";
-        echo "<center>An Error occurred during connection.</center>";
-        echo "\n\t</div>\n</div>";
-        exit();
-    }
-    mt_srand(); //Seed number generator
-    $salt = mt_rand(); //generate salt
-    $salt = sha1($salt);
-    $salt = trim($salt);
-    $pass = htmlspecialchars($_POST['password']);
-    $email = htmlspecialchars($_POST['email']);
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $passHashed = sha1($salt.$pass);
-    for ($i=0; $i<10000; $i++) //Slow Hashing
-    {
-        $passHashed = sha1($passHashed);
-    }
-    $query = "INSERT INTO careerschema.authorizationtable (email, firstname, lastname, hashed_pass, salt, user_type) VALUES ($1,$2,$3,$4,$5,$6)";
-    $state = pg_prepare($conn,"insert_1",$query) ;
-    $queryInsert = pg_execute($conn,"insert_1",array($email, $_POST['firstName'], $_POST['lastName'], $passHashed, $salt,"admin") )  ;
-    if ($queryInsert)
-    {
-        //Kristi:  change to the appropiate action if the admin is inserted successfully
-        header("Location: admin.php");//change this line
-        exit();
-    }
-    else{
-        //Kristi:  change to the appropiate action if the admin is not inserted
-        header("Location: admin.php#failure");//change this line
-        exit();
-    }
-}
 ?>
